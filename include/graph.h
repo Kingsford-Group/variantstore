b8bafcf11ba5b03200ec4f936153250cbeff554f
Prashant Pandey <prashant.prashn@gmail.com>
2019-02-26 15:30:11 -0500

    Adding graph header. Empty right now.

---
+++ include/graph.h
@@ -0,0 +1,22 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  graph.h
+ *
+ *         Author:  Prashant Pandey (), ppandey2@cs.cmu.edu
+ *   Organization:  Carnegie Mellon University
+ *
+ * ============================================================================
+ */
+
+#ifndef __GRAPH_H_
+#define __GRAPH_H_
+
+namespace variantdb {
+
+	class Graph {
+		
+	};
+}
+
+#endif	// __GRAPH_H_
--- include/util.h
+++ include/util.h
@@ -35,24 +35,11 @@
 	{ std::cout << x << std::endl; } \
 } while (0)
 
-namespace popcornfilter {
+namespace variantdb {
 	float cal_time_elapsed(struct timeval* start, struct timeval* end);
 	/* Print elapsed time using the start and end timeval */
 	void print_time_elapsed(std::string desc, struct timeval* start, struct
 													timeval* end);
 
-	std::unordered_map<uint64_t, std::pair<uint64_t, uint64_t>>
-		analyze_stream(uint64_t *vals, uint64_t nvals, uint32_t threshold);
-
-	uint64_t actual_count_at_index(uint64_t *vals, uint64_t key, uint64_t index);
-
-	void induce_special_case(uint64_t *vals, uint32_t threshold, uint64_t
-													 start_idx, uint64_t lifetime, uint32_t num);
-	uint64_t *read_file_disk(std::string file);
-	uint64_t *read_stream_from_disk(std::string file, uint64_t *num_keys);
-	uint64_t get_number_keys(std::string file);
-
-	std::unordered_map<uint64_t, std::pair<uint64_t, uint64_t>>
-		read_stream_log_from_disk(std::string file);
 }
 #endif
