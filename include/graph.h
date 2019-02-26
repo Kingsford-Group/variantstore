4197c0ef94f0d9242b48a08c904571778421e613
Prashant Pandey <prashant.prashn@gmail.com>
2019-02-26 17:26:51 -0500

    partial commit for the graph header class.

--- include/graph.h
+++ include/graph.h
@@ -9,14 +9,47 @@
  * ============================================================================
  */
 
-#ifndef __GRAPH_H_
-#define __GRAPH_H_
+#ifndef __GRAPH_H__
+#define __GRAPH_H__
+
+#include <stdlib.h>
+
+#include <vector>
+
+#include "gqf_cpp.h"
 
 namespace variantdb {
 
-	class Graph {
-		
-	};
+	// An efficient way to store graph topology for sparse (and skewed) DAGs. 
+	// Vertexes are defined as 32-bit integers.
+	// It uses a variable-length encoding from the QF (counter encoding) data
+	// structure to endcode the neighbors of a vertex. 
+	// If there's only one neighbor then it is stored next to the source vertex
+	// in the QF. If there are more than one neighbors then they are stored in a
+	// vector and a pointer is stored next to the source vertex in the QF.
+	template <class key_obj>
+		class Graph {
+			public:
+				typedef uint32_t vertex;
+				typedef std::pair<uint32_t, uint32_t> edge;
+				typedef vector<vertex> vertex_set;
+				typedef vector<edge> edge_set;
+
+				Graph();
+				Graph(uint64_t size);
+
+				int add_edge(vertex s, vertex d);
+
+				vertex_set get_neighbors(vertex s);
+
+				class Iterator {
+
+				};
+
+
+			private:
+				CQF<key_obj> adj_list;
+		};
 }
 
 #endif	// __GRAPH_H_
