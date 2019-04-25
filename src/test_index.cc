0ccdfae050b5431a254aa354537f2fc36fef746e
User Yinjieg <yinjieg@arctic.compbio.cs.cmu.edu>
2019-04-25 01:03:04 -0400

    index.h & test_index.h done with s_info TOCHANGE

--- src/test_index.cc
+++ src/test_index.cc
@@ -9,6 +9,105 @@
 #include <map>
 #include <iterator>
 
+#include "index.h"
+#include "variant_graph.h"
+
+using namespace variantdb;
+
+void print_index_info(const Index &idx, uint64_t len)
+{
+  PRINT("Sequence length is");
+  PRINT("Pos\tNode_id");
+
+  for (uint64_t i=0; i<len; i++)
+  {
+    uint64_t node_id = idx.find(i);
+    PRINT(i << ":" << node_id);
+  }
+  return;
+}
+
+void print_vg_info(VariantGraph& vg, std::string& vcf_file) {
+	PRINT("Graph stats:");
+	PRINT("Chromosome: " << vg.get_chr() << " #Vertices: " << vg.get_num_vertices()
+				<< " Seq length: " << vg.get_seq_length());
+
+	PRINT("Variant Graph nodes:");
+	for (int i = 0; i < 8; i++) {
+		auto bfs = vg.find(0, i);
+		std::cout << "radius: " << i << " -- ";
+		while (!bfs.done()) {
+			std::cout << (*bfs)->vertex_id() << " ";
+			++bfs;
+		}
+		PRINT("");
+	}
+
+	auto itr = vg.find("ref");
+	PRINT("Ref nodes:");
+	while (!itr.done()) {
+		//vg.print_vertex_info(**itr);
+		std::cout << vg.get_sequence(**itr);
+		++itr;
+	}
+
+	PRINT("");
+	PRINT("Samples:");
+	vcflib::VariantCallFile variantFile;
+	variantFile.open(vcf_file);
+	vcflib::Variant var(variantFile);
+
+	// get all samples
+	for (auto sample : variantFile.sampleNames) {
+		PRINT("Sample: " << sample);
+		auto itr = vg.find(sample);
+		while (!itr.done()) {
+			//vg.print_vertex_info(**itr);
+			std::cout << vg.get_sequence(**itr);
+			++itr;
+		}
+    PRINT("");
+	}
+	PRINT("");
+}
+
+
+int
+main ( int argc, char *argv[] )
+{
+if (argc < 2) {
+  fprintf(stderr, "Please specify the reference fasta file and vcf file.\n");
+  exit(1);
+}
+
+//std::string filename = argv[1];
+
+PRINT("Creating variant graph");
+std::string ref_file(argv[1]);
+std::string vcf_file(argv[2]);
+std::vector<std::string> vcfs = {vcf_file};
+VariantGraph vg(ref_file, vcfs);
+
+print_vg_info(vg, vcf_file);
+
+PRINT("Creating Index");
+Index idx(&vg);
+print_index_info(idx, 50);
+
+PRINT("Storing Index");
+idx.serialize("test");
+
+PRINT("Reading Index");
+Index idx2("test");
+print_index_info(idx2, 50);
+
+
+
+
+return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
+
+/*
 int main()
 {
 
@@ -103,3 +202,4 @@ int main()
     serialize(node_list, out);
     return 0;
 }
+*/
