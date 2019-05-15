1d52bf06973668aa37952e486a97c23bd5321cbb
User Yinjieg <yinjieg@arctic.compbio.cs.cmu.edu>
2019-05-15 09:01:45 -0400

    implemented query in sample's coordinate, to be tested

--- include/query.h
+++ include/query.h
@@ -1,4 +1,3 @@
-#include "dot_graph.h"
 #include "index.h"
 #include "variant_graph.h"
 #include <string>
@@ -9,9 +8,12 @@ namespace variantdb {
 // TODO: check validity of inputs
 
 // Do BFS of radius 1 to prev node in ref until sample is found
-Graph::vertex get_prev_vertex_with_sample (VariantGraph *vg, Index *idx,
-                                          uint64_t pos, std::string sample_id,
-                                          uint64_t &ref_pos, uint64_t &sample_pos) {
+Graph::vertex get_prev_vertex_with_sample ( const VariantGraph *vg,
+                                            const Index *idx,
+                                            const uint64_t pos,
+                                            const uint32_t sample_id_idx,
+                                            uint64_t &ref_pos,
+                                            uint64_t &sample_pos) {
   uint64_t cur_pos = pos;
   Graph::vertex v = idx->find(cur_pos);
   Graph::vertex v_find = v;
@@ -25,6 +27,7 @@ Graph::vertex get_prev_vertex_with_sample (VariantGraph *vg, Index *idx,
     Graph::vertex new_v = prev_v;
     while (new_v == prev_v && cur_pos > 1) {
       cur_pos = cur_pos - 1;
+
       new_v = idx->find(cur_pos);
     }
 
@@ -33,7 +36,7 @@ Graph::vertex get_prev_vertex_with_sample (VariantGraph *vg, Index *idx,
     if (cur_pos <= 1) {
       ref_pos = 1;
       v_find = v;
-      vg->get_sample_from_vertex_if_exists(v, "ref", sample_find);
+      vg->get_sample_from_vertex_if_exists(v, 0, sample_find);
       break;
     }
 
@@ -42,9 +45,9 @@ Graph::vertex get_prev_vertex_with_sample (VariantGraph *vg, Index *idx,
 		{
       v = (*it)->vertex_id();
       //DEBUG("check vertex: " << v);
-      if (vg->get_sample_from_vertex_if_exists(v, "ref", sample))
+      if (vg->get_sample_from_vertex_if_exists(v, 0, sample))
       { ref_pos = sample.index(); }
-			if (vg->get_sample_from_vertex_if_exists(v, sample_id, sample_find))
+			if (vg->get_sample_from_vertex_if_exists(v, sample_id_idx, sample_find))
       {
         v_find = v;
         find_sample = true;
@@ -62,13 +65,16 @@ Graph::vertex get_prev_vertex_with_sample (VariantGraph *vg, Index *idx,
 }
 
 // return sequence of sample_id from ref position [x, y)
-std::string query_sample_from_ref (VariantGraph *vg, Index *idx,
-  uint64_t pos_x, uint64_t pos_y, std::string sample_id) {
+std::string query_sample_from_ref ( const VariantGraph *vg, const Index *idx,
+                                    const uint64_t pos_x, const uint64_t pos_y,
+                                    const string sample_id) {
   std::string seq = "";
   uint64_t ref_pos;
   uint64_t sample_pos;
+  uint32_t sample_id_idx; //TODO
   Graph::vertex closest_v = get_prev_vertex_with_sample(vg, idx, pos_x,
-    sample_id, ref_pos, sample_pos);
+                                                        sample_id_idx,
+                                                        ref_pos, sample_pos);
   // Do DFS on the path of the sample, record ref idx along the way
   VariantGraph::VariantGraphPathIterator it = vg->find(closest_v, sample_id);
   bool record_seq = false;
@@ -87,7 +93,7 @@ std::string query_sample_from_ref (VariantGraph *vg, Index *idx,
     {
       Graph::vertex v = (*temp_it)->vertex_id();
       VariantGraphVertex::sample_info sample;
-      if (vg->get_sample_from_vertex_if_exists(v, "ref", sample))
+      if (vg->get_sample_from_vertex_if_exists(v, 0, sample))
       { next_ref_pos = sample.index(); }
       ++temp_it;
     }
@@ -103,7 +109,6 @@ std::string query_sample_from_ref (VariantGraph *vg, Index *idx,
       record_seq = true;
       seq += temp.substr(pos_x - ref_pos);
     } else if (next_ref_pos >= pos_x && next_ref_pos >= pos_y) {
-      DEBUG(pos_x - ref_pos << " " << pos_y - pos_x);
       seq = temp.substr(pos_x - ref_pos, pos_y - pos_x);
       break;
     }
@@ -115,9 +120,51 @@ std::string query_sample_from_ref (VariantGraph *vg, Index *idx,
   return seq;
 }
 
-std::string query_sample_from_sample (uint64_t pos_x,
-  uint64_t pos_y, std::string sample_id){
-  std::string seq;
+std::string query_sample_from_sample ( const VariantGraph *vg, const Index *idx,
+                                       const uint64_t pos_x,
+                                       const uint64_t pos_y,
+                                       const string sample_id ){
+  std::string seq = "";
+  uint64_t ref_pos;
+  uint64_t sample_pos;
+  uint32_t sample_id_idx; //TODO
+  Graph::vertex closest_v = get_prev_vertex_with_sample(vg, idx, pos_x,
+                                                        sample_id_idx,
+                                                        ref_pos, sample_pos);
+  while (sample_pos >= pos_x)
+  {
+    uint64_t pos = ref_pos;
+    closest_v = get_prev_vertex_with_sample(vg, idx, pos, sample_id_idx,
+                                            ref_pos, sample_pos);
+  }
+
+  VariantGraph::VariantGraphPathIterator it = vg->find(closest_v, sample_id);
+  bool record_seq = false;
+  std::string temp;
+
+  while (!it.done()) {
+    temp.assign(vg->get_sequence(*(*it)));
+    DEBUG("Check node: " << (*it)->vertex_id() << ", Reference Index: "
+          << sample_pos << ", Seq " << temp);
+    uint64_t l = (*it)->length();
+    uint64_t next_sample_pos = sample_pos + l;
+
+    if (record_seq == true && next_sample_pos < pos_y) {
+      seq += temp;
+    } else if (record_seq == true && next_sample_pos >= pos_y) {
+      seq += temp.substr(0, pos_y - sample_pos);
+      break;
+    } else if (next_sample_pos >= pos_x && next_sample_pos < pos_y) {
+      record_seq = true;
+      seq += temp.substr(pos_x - sample_pos);
+    } else if (next_sample_pos >= pos_x && next_sample_pos >= pos_y) {
+      seq = temp.substr(pos_x - sample_pos, pos_y - pos_x);
+      break;
+    }
+
+    ++it;
+    sample_pos = next_sample_pos;
+  }
   return seq;
 }
 
