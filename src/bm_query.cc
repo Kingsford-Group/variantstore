38fd8e321903401fa46d26ac927efa556c9ec2ff
User Yinjieg <yinjieg@arctic.compbio.cs.cmu.edu>
2019-06-16 17:32:10 -0400

    benchmark for query

--- src/bm_query.cc
+++ src/bm_query.cc
@@ -26,16 +26,30 @@ std::shared_ptr<spdlog::logger> console;
 
 int main ( int argc, char *argv[] )
 {
+  console = spdlog::default_logger();
 
+  std::string ref_file("/mnt/disk34/user/ppandey/vcfdata/ftp.ensembl.org/pub/grch37/current/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.dna_sm.chromosome.22.fa");
+	std::string vcf_file("/mnt/disk34/user/ppandey/vcfdata/ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz");
+	std::vector<std::string> vcfs = {vcf_file};
+	PRINT("Creating variant graph");
+	VariantGraph vg(ref_file, vcfs);
+
+	PRINT("Creating Index");
+	Index idx(&vg);
+  /*
   // Read from serialized data
   PRINT("Reading chr22 ...");
   auto start = std::chrono::high_resolution_clock::now();
   VariantGraph vg("./chr22");
-  Index idx("./chr22");
+  PRINT("Reading chr22 ...");
+  Index idx("./chrY");
   auto stop = std::chrono::high_resolution_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop-start);
   PRINT("The time taken for loading is: " << duration.count()	<< "seconds");
-
+  */
+  auto start = std::chrono::high_resolution_clock::now();
+  auto stop = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop-start);
   // Take 10 random samples
   std::vector <string> samples;
   uint32_t max = 600;
@@ -48,7 +62,8 @@ int main ( int argc, char *argv[] )
 
   // Generate 10 random positions
   std::vector <uint64_t> positions;
-  uint64_t chr_len = vg.get_seq_length();
+  uint64_t chr_len = vg.get_ref_length();
+  PRINT("chr_len is: " << chr_len);
   uint64_t avg_query_len = chr_len / 10;
   for (auto i=0; i<10; i++)
   {
@@ -62,37 +77,41 @@ int main ( int argc, char *argv[] )
   for (auto sample=samples.begin(); sample!=samples.end();++sample)
   {
     std::string sample_id = *sample;
+    PRINT(sample_id);
     for (auto pos=positions.begin(); pos!=positions.end();++pos)
     {
       uint64_t pos_x = *pos;
       uint64_t pos_y = pos_x + avg_query_len;
+      PRINT(pos_x << "," << pos_y);
       get_sample_var_in_ref(&vg, &idx, pos_x, pos_y, sample_id);
     }
   }
   stop = std::chrono::high_resolution_clock::now();
-  duration = std::chrono::duration_cast<std::chrono::seconds>(stop-start);
+  duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop-start);
   auto avg_time = duration.count() / 100;
   PRINT("The average time taken for get_sample_var_in_ref is: "
-        << avg_time	<< "seconds");
+        << avg_time	<< "milliseconds");
 
   // Benchmark for query query_sample_from_ref
-  PRINT("Benchmark for query query_sample_from_ref ...");
+  PRINT("Benchmark for query_sample_from_ref ...");
   start = std::chrono::high_resolution_clock::now();
   for (auto sample=samples.begin(); sample!=samples.end();++sample)
   {
     std::string sample_id = *sample;
+    PRINT(sample_id);
     for (auto pos=positions.begin(); pos!=positions.end();++pos)
     {
       uint64_t pos_x = *pos;
       uint64_t pos_y = pos_x + avg_query_len;
+      PRINT(pos_x << "," << pos_y);
       query_sample_from_ref(&vg, &idx, pos_x, pos_y, sample_id);
     }
   }
   stop = std::chrono::high_resolution_clock::now();
-  duration = std::chrono::duration_cast<std::chrono::seconds>(stop-start);
+  duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop-start);
   avg_time = duration.count() / 100;
   PRINT("The average time taken for query_sample_from_ref is: "
-        << avg_time	<< "seconds");
+        << avg_time	<< "milliseconds");
 
   // Benchmark for query query_sample_from_ref
   PRINT("Benchmark for query closest_var ...");
@@ -100,18 +119,20 @@ int main ( int argc, char *argv[] )
   for (auto sample=samples.begin(); sample!=samples.end();++sample)
   {
     std::string sample_id = *sample;
+    PRINT(sample_id);
     for (auto pos=positions.begin(); pos!=positions.end();++pos)
     {
       uint64_t pos_x = *pos;
+      PRINT(pos_x);
       Variant var;
   		closest_var (&vg, &idx, pos_x, var);
     }
   }
   stop = std::chrono::high_resolution_clock::now();
-  duration = std::chrono::duration_cast<std::chrono::seconds>(stop-start);
+  duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop-start);
   avg_time = duration.count() / 100;
   PRINT("The average time taken for closest_var is: "
-        << avg_time	<< "seconds");
+        << avg_time	<< "milliseconds");
 
 
   return 0;
