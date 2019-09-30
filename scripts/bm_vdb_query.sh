e48faba9075bb290467b9cc62f3b6a01993cf9ac
Prashant Pandey <ppandey2@cs.cmu.edu>
2019-09-30 09:52:47 -0400

    update vdb query bm script.

--- scripts/bm_vdb_query.sh
+++ scripts/bm_vdb_query.sh
@@ -11,10 +11,13 @@ type="$5"
 pct="$3"
 query_len=$(($len/$pct))
 mode="$6"
+sample="$7"
 echo "Query length: $query_len"
 
+start=16050000
+end=$(($start+$len-$query_len))
 # Generate n random starting positions
-pos=$(echo $(shuf -i 16050000-$len -n $num))
+pos=$(echo $(shuf -i $start-$end -n $num))
 
 
 r1=''
@@ -30,11 +33,12 @@ done
 r1=${r1::-1}
 r2=${r2::-1}
 
+set -x
 if (($type == 3))
 then
   echo "Benchmarking query type $type"
-  ./variantdb query -t $type -p $vdb_file -r $r2 -v 1 -m $mode -o temp.txt
+  ./variantdb query -t $type -p $vdb_file -r $r2 -m $mode -o temp.txt
 else
   echo "Benchmarking query type $type"
-  ./variantdb query -t $type -p $vdb_file -r $r1 -v 1 -m $mode -o temp.txt
+  ./variantdb query -t $type -p $vdb_file -r $r1 -m $mode -s $sample -o temp.txt
 fi
