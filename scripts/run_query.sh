e641372aeceab7e1cc7dd6447a60bbb0f9338aa6
Prashant Pandey <ppandey2@cs.cmu.edu>
2021-03-14 20:48:48 -0400

    Makefile changes

--- scripts/run_query.sh
+++ scripts/run_query.sh
@@ -3,8 +3,8 @@ for i in {1..6}
 		echo $i;
     for j in 10 100 1000
       		do
-			./scripts/bm_vs_query.sh ser/ $j 43185 51304566 $i 0 HG01872 >> query.txt;
-			#./bm_vs_query.sh vs_v4/chr2/ $j 43185 243199373 $i 0 HG01872 >> query.txt;
+			#./scripts/bm_vs_query.sh vs_v1/chr22 $j 43185 51304566 $i 0 HG01872 >> query.txt;
+      ./scripts/bm_vs_query.sh vs_v1/chr2/ $j 43185 243199373 $i 0 HG01872 >> query.txt;
       #./bm_vs_query.sh /dbgap/ckingsf_A018251/ppandey2/tcga-new/vs_luad/chr22/ $j 42776 50818468 $i 0 184:TUMOR >> query_luad.out
       #./bm_vs_query.sh /dbgap/ckingsf_A018251/ppandey2/tcga-new/vs_luad/chr2/ $j 42776 242193529 $i 0 184:TUMOR >> query_luad.out
 		done
